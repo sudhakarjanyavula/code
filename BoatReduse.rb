6576a5eeb2f3707a2cc48d14d76f5ae931397574
sudhakar <sudhakarjanyavula@gmail.com>
2022-07-28 23:05:58 +0530

    BoatReduse.rb

--- BoatReduse.rb
+++ BoatReduse.rb
@@ -20,3 +20,27 @@ end
 
 result =  boatReduse(people, limit)
 puts "No of Boats Required is : #{result}"
+
+##########################################
+# Little Advance Method (space complexity)
+##########################################
+
+# people = [4,3,2,5,1,2]
+# limit = 6
+# def boatReduse (people, limit)
+#     s = people.sort
+#     left = 0
+#     right = people.length-1
+#     boat = 0
+#     while left <= right
+#         if (s[left] + s[right]) <= limit   
+#             left += 1
+#         end
+#         boat += 1
+#         right -= 1
+#     end
+#     return boat
+# end
+
+# result =  boatReduse(people, limit)
+# puts "No of Boats Required is : #{result}"
